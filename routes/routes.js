f74e66e0b4c42980ff5d362508c10f66c7e38716
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2016-02-24 14:10:08 +1100

    more routes added

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,17 @@
 var appRouter = function(app) {
 
+    var patronList = {
+        "patronid": "0002000",
+        "name": "test patron",
+        "class": "FULL"
+    }
+
     app.get("/api", function(req, res) {
          res.send("This is a test node app running in Azure Cloud.");
+    });
+
+     app.get("/api/patronlist", function(req, res) {
+         return res.send(patronList);
     });
 
 }
